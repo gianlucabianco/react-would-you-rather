0b8703f682ba46ec73bdd1d91ad6eac5720b9978
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-25 17:16:58 +0200

    feature: Login component first draft

---
+++ src/components/UserSelect.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import users from '../users';
+import { Avatar } from './Avatar.js';
+
+import './UserSelect.scss';
+
+class UserSelect extends React.Component {
+
+    state = {
+        users,
+        userId: '',
+        userName: '',
+        userUrl: '',
+        isSelectOpen: false,
+    };
+
+    toggleDropdown = isOpen => {
+        this.setState(
+            {
+                isSelectOpen: isOpen,
+            }
+        );
+    };
+
+    blurUserSelect = () => {
+        this.state.isSelectOpen
+            && this.setState(
+                {
+                    isSelectOpen: false,
+                }
+            );
+    };
+
+    handleDropdownClick = () => {
+        this.toggleDropdown(
+            ! this.state.isSelectOpen
+        );
+    };
+
+    render() {
+
+        const {
+            users,
+            userId,
+            userName,
+            userUrl,
+            isSelectOpen,
+        } = this.state;
+
+        return (
+            <div
+                className="user-select"
+                tabIndex="0"
+                onBlur={ this.blurUserSelect }
+            >
+                <div
+                    className="dropdown"
+                    onClick={ this.handleDropdownClick }
+                >
+                    {
+                        (! isSelectOpen && ! userId) || isSelectOpen
+                            ? <span>Please select a User</span>
+                            : <>
+                                <Avatar url={userUrl} />
+                                <span>{userName}</span>
+                            </>
+                    }
+                    {/* TODO: SELECT-BTN SHOULD BE A COMPONENT */}
+                    {
+                        ! isSelectOpen
+                            ? <span className="select-button" >
+                                &#9660;
+                            </span>
+                            : <span className="select-button" >
+                                &#x25B2;
+                            </span>
+                    }
+                </div>
+                <div className="optionWrapper">
+                    {
+                        isSelectOpen
+                        && users.map(
+                            user => (
+                                <div
+                                    className="userOption"
+                                    key={user.id}
+                                    onClick={
+                                        () => {
+                                            this.setState(
+                                                {
+                                                    userId: user.id,
+                                                    userName: user.name,
+                                                    userUrl: user.avatarUrl,
+                                                    isSelectOpen: false,
+                                                }
+                                            );
+                                        }
+                                    }
+                                >
+                                    <Avatar url={user.avatarUrl} />
+                                    <span>{user.name}</span>
+                                </div>
+                            )
+                        )
+                    }
+                </div>
+            </div>
+        );
+    }
+
+};
+
+export { UserSelect };
