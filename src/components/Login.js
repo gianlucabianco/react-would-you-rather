8fbcfd68a1dbb92369a8223e1981a8e391e0ad37
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-25 17:17:54 +0200

    feat: Login component uses now UserSelect component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { UserSelect } from './UserSelect';
 
 import './Login.scss';
 
 class Login extends React.Component {
 
     render() {
-
         return (
             <div className="login">
-                please login
+                <h1>Welcome to the Would you rather login page</h1>
+                <p>Please sign in to continue</p>
+                <UserSelect />
             </div>
         );
-
-    };
+    }
 
 };
 
