05c1357553c35536fd2020282841d8c7338cd9e4
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-25 11:56:07 +0200

    feature: Login component init

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Login.scss';
+
+class Login extends React.Component {
+
+    render() {
+
+        return (
+            <div className="login">
+                please login
+            </div>
+        );
+
+    };
+
+};
+
+export { Login };
