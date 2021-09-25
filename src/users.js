07f7c28a13f26fba88026da9cef6261a334c95c5
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-25 12:14:44 +0200

    chore: ids added to users.js

--- src/users.js
+++ src/users.js
@@ -1,39 +1,48 @@
 const users = [
     {
         avatarUrl: 'https://picsum.photos/id/1027/200/300',
-        name: 'Jean-Paul Sartre'
+        name: 'Jean-Paul Sartre',
+        id: '1',
     },
     {
         avatarUrl: 'https://picsum.photos/id/103/200/300',
-        name: 'Slavoj Žižek'
+        name: 'Slavoj Žižek',
+        id: '2',
     },
     {
         avatarUrl: 'https://picsum.photos/id/104/200/300',
-        name: 'Michel Foucault'
+        name: 'Michel Foucault',
+        id: '3',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1049/200/300',
-        name: 'Noam Chomsky'
+        name: 'Noam Chomsky',
+        id: '4',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1006/200/300',
-        name: 'Allama Iqbal'
+        name: 'Allama Iqbal',
+        id: '5',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1010/200/300',
-        name: 'Alain Locke'
+        name: 'Alain Locke',
+        id: '6',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1011/200/300',
-        name: 'Mo-zi'
+        name: 'Mo-zi',
+        id: '7',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1020/200/300',
-        name: 'Alfred Schütz'
+        name: 'Alfred Schütz',
+        id: '8',
     },
     {
         avatarUrl: 'https://picsum.photos/id/1025/200/300',
-        name: 'Henry Sidgwick'
+        name: 'Henry Sidgwick',
+        id: '9',
     },
 ];
 
