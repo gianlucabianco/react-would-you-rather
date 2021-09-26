ef24a3946168bf6e113c2f546a2410a447763b58
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-26 14:55:48 +0200

    feature: login component has now login btn , first draft

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,23 @@ import './Login.scss';
 
 class Login extends React.Component {
 
+    state = {
+        userId: '',
+    };
+
     render() {
+
+        const { userId } = this.state;
+        const btnClasses = `login__button ${! userId ? 'disabled' : ''}`;
+
+        console.log({btnClasses, userId});
+
         return (
             <div className="login">
                 <h1>Welcome to the Would you rather login page</h1>
                 <p>Please sign in to continue</p>
                 <UserSelect />
+                <button className={ btnClasses }>Login</button>
             </div>
         );
     }
