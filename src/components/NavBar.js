7d214f12db271d82dbc388083a9322b8a6b52e7b
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-09-30 08:58:46 +0200

    feature: login/logout button first draf

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Avatar } from './Avatar';
 
 import './NavBar.scss';
 
 class NavBar extends React.Component {
+
+    state = {
+        isLoggedIn: true,
+        // users,
+        userId: '1',
+        userName: 'Jean-Paul Sartre',
+        userUrl: 'https://picsum.photos/id/104/200/300',
+    }
+
 
     mockMethod = item => {
         console.log(
@@ -34,10 +44,26 @@ class NavBar extends React.Component {
                     >
                         Ranking
                     </li>
-                    <li
-                        onClick={ () => this.mockMethod( 'login' ) }
-                    >
-                        login
+                    <li onClick={ () => this.mockMethod( 'login' ) } >
+                        {/* TODO: this should be a component */}
+                        {
+                            ! this.state.isLoggedIn
+                                ? 'Login'
+                                : <div className="login-button">
+                                    <div className="avatar-wrapper" >
+                                        <Avatar
+                                            url={ this.state.userUrl }
+                                            size={'40px'}
+                                        />
+                                        <span className="user-name">
+                                            { this.state.userName }
+                                        </span>
+                                    </div>
+                                    <span className="logout-button">
+                                        Logout
+                                    </span>
+                                </div>
+                        }
                     </li>
                 </ul>
             </div>
