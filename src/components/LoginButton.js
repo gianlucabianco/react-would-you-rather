7d14e59bf1bd2f2b3ca56170750c3d05f7a501c7
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-10-01 17:48:58 +0200

    feature: login button is now component

---
+++ src/components/LoginButton.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+// TODO: add propTypes
+// import PropTypes from 'prop-types';
+
+import './LoginButton.scss';
+import { Avatar } from './Avatar';
+
+class LoginButton extends Component {
+
+    render() {
+
+        const {
+            userUrl,
+            userName,
+            isLoggedIn,
+        } = this.props;
+
+        return (
+            <>
+                {
+                    ! isLoggedIn
+                        ? 'Login'
+                        : <div className="login-button">
+                            <div className="avatar-wrapper" >
+                                <Avatar
+                                    url={ userUrl }
+                                    size={'40px'}
+                                />
+                                <span className="user-name">
+                                    { userName }
+                                </span>
+                            </div>
+                            <span className="logout-button">
+                                Logout
+                            </span>
+                        </div>
+                }
+            </>
+        );
+
+    };
+
+};
+
+// TODO: Add propTypes
+
+export { LoginButton };
