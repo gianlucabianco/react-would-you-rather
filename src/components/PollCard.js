4f22a25f6fb403f1085a2afccbd3fd5ad19a9d68
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-11-02 09:20:36 +0100

    chore: PollCard style import statement adjusted

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './PollCard.scss';
+import './PollCard.css';
 
 import { Option } from './Option.js';
 import { Avatar } from './Avatar.js';
