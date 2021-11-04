8c21e466ecb387dafc96fab23a1f3a83c601a837
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-11-04 09:00:53 +0100

    chore: PollButton style import statement adjusted

--- src/components/PollButton.js
+++ src/components/PollButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import './PollButton.scss';
+import './PollButton.css';
 
 class PollButton extends Component {
     
