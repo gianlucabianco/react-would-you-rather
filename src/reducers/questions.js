d7e3bd5544f997448e0d2785deb1d4527fbf96d1
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-04 12:47:25 +0100

    feat: questions.js is now reducer

---
+++ src/reducers/questions.js
@@ -0,0 +1,16 @@
+import { GET_QUESTIONS } from '../actions/questions';
+
+export default function questions(
+    state = {},
+    action,
+) {
+  switch (action.type) {
+    case GET_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions
+      };
+    default:
+      return state;
+  }
+}
