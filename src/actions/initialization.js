e99491b8e5f0e6131b0cdd1f7915c3f5c2f3d4a5
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-04 12:42:27 +0100

    feat: initialization.js is now action

---
+++ src/actions/initialization.js
@@ -0,0 +1,19 @@
+import { initializeData } from '../API/api.js';
+import { getQuestions } from './questions.js';
+import { getUsers } from './users.js';
+
+export function handleInitializationData() {
+  return dispatch => {
+    return initializeData().then(
+        (
+            {
+                users,
+                questions,
+            }
+        ) => {
+            dispatch( getQuestions( questions ) );
+            dispatch( getUsers( users ) );
+        }
+    );
+  };
+}
