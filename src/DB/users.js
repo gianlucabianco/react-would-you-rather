781b44b335f35f47ed027cf4c162c319560efeca
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-05 13:14:15 +0100

    feature: users.js fake DB adjustment

--- src/DB/users.js
+++ src/DB/users.js
@@ -1,35 +1,96 @@
 export const users = {
-  sarahedo: {
-    id: 'sarahedo',
-    name: 'Sarah Edo',
-    avatarURL: '',
+  "cpa4gc93t0kjtu34dz07s": {
+    id: "cpa4gc93t0kjtu34dz07s",
+    name: 'Jean-Paul Sartre',
+    avatarURL: 'https://picsum.photos/id/1027/200/300',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
       "am8ehyc8byjqgar0jgpub9": 'optionTwo',
-      "loxhs1bqm25b708cmbf3g": 'optionTwo'
     },
-    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+    questions: [
+      '8xf0y6ziyjabvozdd253nd',
+      'am8ehyc8byjqgar0jgpub9',
+    ],
   },
-  tylermcginnis: {
-    id: 'tylermcginnis',
-    name: 'Tyler McGinnis',
-    avatarURL: '',
+  "y66vmt5eelkljlbk0xh4m": {
+    id: "y66vmt5eelkljlbk0xh4m",
+    name: 'Slavoj Žižek',
+    avatarURL: 'https://picsum.photos/id/103/200/300',
+    answers: {
+      "vthrdm985a262al8qx3do": 'optionTwo',
+      "xj352vofupe1dqz9emx13r": 'optionOne',
+    },
+    questions: [
+      'xj352vofupe1dqz9emx13r',
+    ],
+  },
+  "3fv2y6608ym68yrd9qdqz4": {
+    id: "3fv2y6608ym68yrd9qdqz4",
+    name: 'Michel Foucault',
+    avatarURL: 'https://picsum.photos/id/104/200/300',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
     },
-    questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+    questions: [
+      'loxhs1bqm25b708cmbf3g',
+      'vthrdm985a262al8qx3do',
+    ],
   },
-  johndoe: {
-    id: 'johndoe',
-    name: 'John Doe',
-    avatarURL: '',
+  /* EMPTY USERS, TODO: populate these */
+  "ya3b29wa7ajqb0vkk1otaq": {
+    id: "ya3b29wa7ajqb0vkk1otaq",
+    name: 'Noam Chomsky',
+    avatarURL: 'https://picsum.photos/id/1049/200/300',
     answers: {
-      "xj352vofupe1dqz9emx13r": 'optionOne',
-      "vthrdm985a262al8qx3do": 'optionTwo',
-      "6ni6ok3ym7mf1p33lnez": 'optionTwo'
     },
-    questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
-  }
+    questions: [
+    ],
+  },
+  "wzzlue8e09pl1dvwwr29": {
+    id: "wzzlue8e09pl1dvwwr29",
+    name: 'Allama Iqbal',
+    avatarURL: 'https://picsum.photos/id/1006/200/300',
+    answers: {
+    },
+    questions: [
+    ],
+  },
+  "h0ypf0auy18scdsizb734": {
+    id: "h0ypf0auy18scdsizb734",
+    name: 'Alain Locke',
+    avatarURL: 'https://picsum.photos/id/1010/200/300',
+    answers: {
+    },
+    questions: [
+    ],
+  },
+  "y94pr6fhjcdritsia4s5": {
+    id: "y94pr6fhjcdritsia4s5",
+    name: 'Mo-zi',
+    avatarURL: 'https://picsum.photos/id/1011/200/300',
+    answers: {
+    },
+    questions: [
+    ],
+  },
+  "adgg9ikjtrhk880lf4zqxd": {
+    id: "adgg9ikjtrhk880lf4zqxd",
+    name: 'Alfred Schütz',
+    avatarURL: 'https://picsum.photos/id/1020/200/300',
+    answers: {
+    },
+    questions: [
+    ],
+  },
+  "w0f9k6btjymgov9yobm3f": {
+    id: "w0f9k6btjymgov9yobm3f",
+    name: 'Henry Sidgwick',
+    avatarURL: 'https://picsum.photos/id/1025/200/300',
+    answers: {
+    },
+    questions: [
+    ],
+  },
 }
