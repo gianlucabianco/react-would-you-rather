ad0629312e76e0fa893f71ffd6dfecd0da391020
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-05 13:15:14 +0100

    feature: questions.js fake DB adjustment

--- src/DB/questions.js
+++ src/DB/questions.js
@@ -1,80 +1,80 @@
 export const questions = {
   "8xf0y6ziyjabvozdd253nd": {
     id: '8xf0y6ziyjabvozdd253nd',
-    author: 'sarahedo',
+    author: 'cpa4gc93t0kjtu34dz07s',
     timestamp: 1467166872634,
     optionOne: {
-      votes: ['sarahedo'],
-      text: 'have horrible short term memory',
+      votes: ['cpa4gc93t0kjtu34dz07s'],
+      text: 'go into the past and meet your ancestors',
     },
     optionTwo: {
       votes: [],
-      text: 'have horrible long term memory'
+      text: 'go into the future and meet your great-great grandchildren'
     }
   },
   "6ni6ok3ym7mf1p33lnez": {
     id: '6ni6ok3ym7mf1p33lnez',
-    author: 'johndoe',
+    author: 'y66vmt5eelkljlbk0xh4m',
     timestamp: 1468479767190,
     optionOne: {
       votes: [],
-      text: 'become a superhero',
+      text: 'have more time',
     },
     optionTwo: {
-      votes: ['johndoe', 'sarahedo'],
-      text: 'become a supervillain'
+      votes: ['y66vmt5eelkljlbk0xh4m', 'cpa4gc93t0kjtu34dz07s'],
+      text: 'more money'
     }
   },
   "am8ehyc8byjqgar0jgpub9": {
     id: 'am8ehyc8byjqgar0jgpub9',
-    author: 'sarahedo',
+    author: 'cpa4gc93t0kjtu34dz07s',
     timestamp: 1488579767190,
     optionOne: {
       votes: [],
-      text: 'be telekinetic',
+      text: 'have a rewind button',
     },
     optionTwo: {
-      votes: ['sarahedo'],
-      text: 'be telepathic'
+      votes: ['cpa4gc93t0kjtu34dz07s'],
+      text: 'a pause button on your life'
     }
   },
   "loxhs1bqm25b708cmbf3g": {
     id: 'loxhs1bqm25b708cmbf3g',
-    author: 'tylermcginnis',
+    author: '3fv2y6608ym68yrd9qdqz4',
     timestamp: 1482579767190,
     optionOne: {
       votes: [],
-      text: 'be a front-end developer',
+      text: 'be able to talk with the animals',
     },
     optionTwo: {
-      votes: ['sarahedo'],
-      text: 'be a back-end developer'
+      votes: ['cpa4gc93t0kjtu34dz07s'],
+      text: 'speak all foreign languages'
     }
   },
   "vthrdm985a262al8qx3do": {
     id: 'vthrdm985a262al8qx3do',
-    author: 'tylermcginnis',
+    author: '3fv2y6608ym68yrd9qdqz4',
     timestamp: 1489579767190,
     optionOne: {
-      votes: ['tylermcginnis'],
-      text: 'find $50 yourself',
+      votes: ['3fv2y6608ym68yrd9qdqz4'],
+      text: 'win the lottery',
     },
     optionTwo: {
-      votes: ['johndoe'],
-      text: 'have your best friend find $500'
+      votes: ['y66vmt5eelkljlbk0xh4m'],
+      text: 'live twice as long'
     }
   },
   "xj352vofupe1dqz9emx13r": {
     id: 'xj352vofupe1dqz9emx13r',
-    author: 'johndoe',
+    author: 'y66vmt5eelkljlbk0xh4m',
     timestamp: 1493579767190,
     optionOne: {
-      votes: ['johndoe'],
-      text: 'write JavaScript',
+      votes: ['y66vmt5eelkljlbk0xh4m'],
+      text: 'be without internet for a week',
     },
     optionTwo: {
-      votes: ['tylermcginnis'],
-      text: 'write Swift'
+      votes: ['3fv2y6608ym68yrd9qdqz4'],
+      text: 'without your phone'
     }
   },
 }
