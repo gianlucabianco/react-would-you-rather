f81b5826ec22af578c545ea560170f053c8ee273
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-14 08:56:17 +0100

    feat: credentials.js is now fake DB

---
+++ src/DB/credentials.js
@@ -0,0 +1,7 @@
+/*
+This is a fake database that stores the credentials for the user.
+In a real projects, this would be stored in a database.
+When updated, this file is not pushed to the repository.
+*/
+
+export const credentials = [ ];
