687482947b7e5ad0a21ebe2ab3b4fb1f1bc99505
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-15 09:15:37 +0100

    chore: linting and comment

--- src/DB/credentials.js
+++ src/DB/credentials.js
@@ -1,7 +1,7 @@
 /*
 This is a fake database that stores the credentials for the user.
 In a real projects, this would be stored in a database.
-When updated, this file is not pushed to the repository.
+When updated, this file is not pushed to the repository (gitignore).
 */
 
-export const credentials = [ ];
+export const credentials = [];
