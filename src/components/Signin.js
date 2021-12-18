59cf63b49e0a0d0d613b5ad44f198a4b2a318041
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-18 16:46:52 +0100

    feat: Signin component initial draft

---
+++ src/components/Signin.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Signin.css';
+
+class Signin extends React.Component {
+
+    state = {
+        // userId: '',
+        userName: '',
+        password: '',
+        // userUrl: '',
+        // isSelectOpen: false,
+    };
+
+    handleUserInput = e => {
+        e.preventDefault();
+        this.setState(
+            {
+                userName: e.target.value,
+                // TODO: handle userId && debounce
+            }
+        );
+        console.log({userName: this.state.userName});
+    };
+    onPasswordChange = e => {
+        e.preventDefault();
+        this.setState(
+            {
+                password: e.target.value,
+            }
+        );
+
+        console.log({password: this.state.password});
+    };
+
+    onSignin = e => {
+        e.preventDefault();
+        console.log({msg: 'sign in!'});
+    };
+
+    render() {
+
+        const {
+            userName,
+            password,
+        } = this.state;
+
+        return (
+            <div className="signin-form">
+                <input
+                    className="signin-input"
+                    type="text"
+                    placeholder="username"
+                    value={userName}
+                    onChange={e => this.handleUserInput(e)}
+                />
+                <input
+                    className="signin-input"
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={e => this.onPasswordChange(e)}
+                />
+                <button
+                    className="signin-button"
+                    onClick={e => this.onSignin(e)}
+                >
+                    Sign in
+                </button>
+                {/* TODO: actions! */}
+                <div className="signin-actions">
+                    <a href="/forgot">Forgot your password?</a>
+                    <a href="/signup">Sign up</a>
+                </div>
+            </div>
+        );
+    };
+}
+
+export default Signin;
