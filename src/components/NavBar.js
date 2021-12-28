5bff72ed4eb80d48f759c6b252811befac7aa67c
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-28 16:48:06 +0100

    feat: user prop comes now from store, TODOs added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,29 +5,30 @@ import './NavBar.css';
 
 class NavBar extends React.Component {
 
-    state = {
-        isLoggedIn: true,
-        // users,
-        userId: '1',
-        userName: 'Jean-Paul Sartre',
-        userUrl: 'https://picsum.photos/id/104/200/300',
-    }
-
+    // TODO: mock method, this should become ROUTING
     mockMethod = item => {
         console.log(
             {
-                item,
+                TODO: item,
             },
         );
     }
 
+    onLogout = () => {
+        this.props.onLogout();
+    };
+
     render() {
 
         const {
-            isLoggedIn,
-            userName,
-            userUrl,
-        } = this.state;
+            user
+        } = this.props;
+        
+        const isLoggedIn = !!(user
+            && Object.keys(user).length);
+
+        const name = user && user.name || '';
+        const avatarURL = user && user.avatarURL || '';
 
         return (
             <div
@@ -43,11 +44,11 @@ class NavBar extends React.Component {
                     <li onClick={ () => this.mockMethod( 'Ranking' ) }>
                         Ranking
                     </li>
-                    <li onClick={ () => this.mockMethod( 'login' ) } >
+                    <li onClick={ () => {isLoggedIn && this.onLogout()} } >
                         <LoginButton
                             isLoggedIn={ isLoggedIn }
-                            userName={ userName }
-                            userUrl={ userUrl }
+                            userName={ name }
+                            userUrl={ avatarURL }
                         />
                     </li>
                 </ul>
