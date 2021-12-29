5006d00cf0b5809932e77cfa30aaa83b93fb156c
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-29 15:34:15 +0100

    feat: Leaderboard.js basic component init

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './Leaderboard.css';
+
+class Leaderboard extends React.Component {
+    render() {
+        return (
+            <div className="leaderboard">
+                <h1>Leaderboard</h1>
+            </div>
+        );
+    }
+}
+
+export default Leaderboard;
