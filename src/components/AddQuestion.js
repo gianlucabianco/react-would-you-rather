1f35f26013620dcc34917dbdfa1ee5e6ba5a1b83
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-30 12:12:49 +0100

    chore: NewQuestion is now AddQuestion

---
+++ src/components/AddQuestion.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddQuestion extends React.Component {
+    render() {
+        return (
+            <div className="new-question">
+                <h1>Add Question</h1>
+            </div>
+        );
+    }
+}
+
+export default AddQuestion;
