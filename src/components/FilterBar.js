eb5352be5b89847889400b105f36123e5eafa421
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-30 13:00:13 +0100

    feat: notAnsered/answered onToggle events are now handled

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './FilterBar.css';
 
 class FilterBar extends React.Component {
+    
+    onToggleAnswered = () => {
+        this.props.onToggleAnswered();
+    }
+
+    onToggleNotAnswered = () => {
+        this.props.onToggleNotAnswered();
+    }
 
     render() {
 
@@ -13,8 +21,18 @@ class FilterBar extends React.Component {
         return (
             <div className="filter-bar">
                 <span>Active filters:</span>
-                <button className={`filter-button not-answered ${! isNotAnsweredToggled ? 'off' : ''}`}>not answered</button>
-                <button className={`filter-button not-answered ${! isAnsweredToggled ? 'off' : ''}`}>answered</button>
+                <button
+                    className={`filter-button not-answered ${! isNotAnsweredToggled ? 'off' : ''}`}
+                    onClick={() => this.props.onToggleNotAnswered()}
+                >
+                    not answered
+                </button>
+                <button
+                    className={`filter-button answered ${! isAnsweredToggled ? 'off' : ''}`}
+                    onClick={() => this.props.onToggleAnswered()}
+                >
+                    answered
+                </button>
             </div>
         )
     }
