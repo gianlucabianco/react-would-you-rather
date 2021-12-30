8070d802b0854f0430fd8713b6471de17ae39799
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-30 12:50:30 +0100

    feat: FilterBar buttons classes are now dynamic

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './FilterBar.css';
 
 class FilterBar extends React.Component {
+
     render() {
+
+        const {
+            isNotAnsweredToggled,
+            isAnsweredToggled,
+        } = this.props;
+
         return (
             <div className="filter-bar">
                 <span>Active filters:</span>
-                <button className="filter-button not-answered">not answered</button>
-                <button className="filter-button answered off">answered</button>
+                <button className={`filter-button not-answered ${! isNotAnsweredToggled ? 'off' : ''}`}>not answered</button>
+                <button className={`filter-button not-answered ${! isAnsweredToggled ? 'off' : ''}`}>answered</button>
             </div>
         )
     }
