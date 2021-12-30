d493ae16a29315da91029d8a9bc36f0e1d717d17
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-30 12:09:55 +0100

    feat: home template and BL lives now inside Home.js component

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ import './App.css';
 import { handleInitializationData } from '../actions/initialization.js';
 import { setAuthUser } from '../actions/authUser.js';
 
-import PollCard from './PollCard.js';
 import NavBar from './NavBar.js';
+import Home from './Home.js';
 import AuthWrapper from './AuthWrapper';
 import Leaderboard from './Leaderboard';
 import NewQuestion from './NewQuestion';
@@ -20,38 +20,6 @@ class App extends Component {
     this.props.handleInitializationData();
   };
 
-  getPercentages = (
-    optionOneVotes,
-    optionTwoVotes
-  ) => {
-
-    const totalVotes = optionOneVotes + optionTwoVotes;
-
-    const optionOnePercentage = Math.round(
-      optionOneVotes / totalVotes * 100
-    );
-
-    const optionTwoPercentage = Math.round(
-      optionTwoVotes / totalVotes * 100
-    );
-
-    return [
-      optionOnePercentage,
-      optionTwoPercentage,
-    ]
-  }
-
-  // TODO: mock method, this should become getReveal()
-  randomizeReveal = () => {
-
-    return Math.floor(
-      (
-        Math.random() * 2
-      ) + 1
-    ) % 2 === 0;
-
-  }
-
   handleLogout = () => {
     this.props.setAuthUser(null);
   }
@@ -62,47 +30,6 @@ class App extends Component {
       authUser,
       users,
     } = this.props;
-
-    const questions = Object.values(this.props.questions);
-
-    const cardsData = ! questions.length
-    ? []
-    : questions.map(
-      question => {
-
-        const user = Object.values(users).find(
-          user => user.id === question.author
-        );
-
-        if(
-          !user
-        ) {
-          return {};
-        }
-
-        const {
-          avatarURL,
-          name,
-          id,
-        } = user;
-
-        return {
-          avatarURL,
-          name,
-          id,
-          color: '#ffffff',
-          isRevealed: this.randomizeReveal(), // TODO: true if the user answer this poll
-          percentages: this.getPercentages(
-            question.optionOne.votes.length,
-            question.optionTwo.votes.length,
-          ),
-          options: [
-            question.optionOne.text,
-            question.optionTwo.text,
-          ],
-        }
-      }
-    );
 
     return (
       <Router>
@@ -125,23 +52,9 @@ class App extends Component {
                 </Route>
               </Switch>
               : <Switch>
-                <Route exact path="/">
-                  {
-                    cardsData.map(
-                      (card, index) => <PollCard
-                        color={card.color}
-                        url={card.avatarURL}
-                        name={card.name}
-                        isRevealed={card.isRevealed}
-                        percentages={card.percentages}
-                        options={card.options}
-                        key={card.name + index}
-                      />
-                    )
-                  }
-                </Route>
+                <Route exact path="/" component={Home} />
                 <Route path="/leaderboard" component={Leaderboard} />
-                <Route path="/add-question" component={NewQuestion} />
+                <Route path="/add-question" component={NewQuestion} /> // TODO: from '/add-question' to '/add'
                 <Route component={ErrorPage} />
               </Switch>
             }
