f9f8386c59254a9b48f30fa2b83b5b724d069c01
Gianluca Bianco <ciao@gianlucabianco.dev>
2021-12-30 12:04:56 +0100

    feat: Home.js is now component

---
+++ src/components/Home.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import PollCard from './PollCard.js';
+
+class Home extends React.Component {
+
+    getPercentages = (
+        optionOneVotes,
+        optionTwoVotes
+    ) => {
+
+        const totalVotes = optionOneVotes + optionTwoVotes;
+
+        const optionOnePercentage = Math.round(
+        optionOneVotes / totalVotes * 100
+        );
+
+        const optionTwoPercentage = Math.round(
+        optionTwoVotes / totalVotes * 100
+        );
+
+        return [
+        optionOnePercentage,
+        optionTwoPercentage,
+        ]
+    }
+
+    // TODO: mock method, this should become getReveal()
+    randomizeReveal = () => {
+
+        return Math.floor(
+        (
+            Math.random() * 2
+        ) + 1
+        ) % 2 === 0;
+
+    }
+    render() {
+
+        const { users, } = this.props;
+
+        const questions = Object.values(this.props.questions);
+
+        const cardsData = ! questions.length
+        ? []
+        : questions.map(
+          question => {
+    
+            const user = Object.values(users).find(
+              user => user.id === question.author
+            );
+    
+            if(
+              !user
+            ) {
+              return {};
+            }
+    
+            const {
+              avatarURL,
+              name,
+              id,
+            } = user;
+    
+            return {
+              avatarURL,
+              name,
+              id,
+              color: '#ffffff',
+              isRevealed: this.randomizeReveal(), // TODO: true if the user answer this poll
+              percentages: this.getPercentages(
+                question.optionOne.votes.length,
+                question.optionTwo.votes.length,
+              ),
+              options: [
+                question.optionOne.text,
+                question.optionTwo.text,
+              ],
+            }
+          }
+        );
+
+        return (
+            <div className="home">
+                {
+                    cardsData.map(
+                      (card, index) => <PollCard
+                        color={card.color}
+                        url={card.avatarURL}
+                        name={card.name}
+                        isRevealed={card.isRevealed}
+                        percentages={card.percentages}
+                        options={card.options}
+                        key={card.name + index}
+                      />
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (
+    {
+      users,
+      questions,
+    }
+  ) => {
+    return {
+      users,
+      questions,
+    };
+  }
+  
+  export default connect(
+    mapStateToProps,
+  )(Home);
