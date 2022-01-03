1325b275c3eeb62c1fb8b69bdf5eadcec65b5c07
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-03 13:21:27 +0100

    feat: leaderboard.js is now component

---
+++ src/components/LeaderboardCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Avatar from './Avatar';
+import './LeaderboardCard.css';
+
+class LeaderboardCard extends React.Component {
+
+    render() {
+
+        const {
+            name,
+            avatarURL,
+            scoreAnsered,
+            scoreAsked,
+            totalScore,
+        } = this.props;
+
+        return (
+            <div className="leaderboard-card">
+                <div className="leaderboard-left">
+                    <Avatar
+                        url={avatarURL}
+                    />
+                    <span>
+                        {name}
+                    </span>
+                </div>
+                <div className="leaderboard-right">
+                    <div className="leaderboard-right-top">
+                    {/* TODO: handle rendering condition and dynamic className for placing (index + 1) */}
+                        <div className="leaderboard-tag">
+                            <span>3</span> place
+                        </div>
+                    </div>
+                    <div className="leaderboard-right-bottom">
+                        <span>
+                            Number of answer: {scoreAnsered}
+                        </span>
+                        <span>
+                            Number of questions: {scoreAsked}
+                        </span>
+                        {/* TODO: this should be styled via css only */}
+                        <span style={{fontWeight: '800'}}>
+                            Total score: {totalScore}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LeaderboardCard;
