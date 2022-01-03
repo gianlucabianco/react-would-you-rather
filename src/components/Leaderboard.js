c6b8c3ed0646969fb44567ab8db1a8e7696dc7f5
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-03 17:22:04 +0100

    feat: placing is now passed as prop from Leaderboard to LeaderboardCard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -34,13 +34,14 @@ class Leaderboard extends React.Component {
                 <h1>Leaderboard</h1>
                 {
                     usersScoring.map(
-                        user => <LeaderboardCard
+                        (user, index) => <LeaderboardCard
                             name={user.name}
                             avatarURL={user.avatarURL}
                             scoreAnsered={user.scoreAnsered}
                             scoreAsked={user.scoreAsked}
                             totalScore={user.totalScore}
                             key={user.id}
+                            placing={index + 1}
                         />
                     )
                 }
