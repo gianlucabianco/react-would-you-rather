1c197ffa83a868995c8b8093da0dfe99326955df
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-07 10:16:42 +0100

    feat: optionStyle and overlayStyle are now dynamic onOptionClick control is now moved inside Option.js component.

--- src/components/Option.js
+++ src/components/Option.js
@@ -6,28 +6,54 @@ import './Option.css';
 class Option extends React.Component {
 
     onOptionClick = () => {
-        this.props.onOptionClick( this.props.option );
+        this.props.isAnswerPage
+        && this.props.onOptionClick( this.props.option );
     };
+
+    getOptionBackgroundColor = (
+        isAnswerPage,
+        color,
+        isSelected,
+    ) => {
+
+        if ( ! isAnswerPage )
+            return color;
+
+        return isSelected ? color : '#11ffee00';
+    };
+
 
     render() {
 
         const {
             content,
             color,
+            isAnswerPage,
+            isSelected,
         } = this.props;
+
+        const optionStyle = {
+            backgroundColor: this.getOptionBackgroundColor(
+                isAnswerPage,
+                color,
+                isSelected,
+            ),
+            transition: 'background-color 0.5s ease',
+        };
+
+        const overlayStyle = {
+            backgroundColor: isAnswerPage ? color : 'white',
+        };
 
         return (
             <div
                 className="option"
-                style={
-                    {
-                        backgroundColor: color
-                    }    
-                }
+                style={ optionStyle }
             >
                 <p>{content}</p>
                 <div
                     className={ 'option-overlay' }
+                    style={ overlayStyle }
                     onClick={ this.onOptionClick }
                 />
             </div>
