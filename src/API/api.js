fa27edacc673726c98502eb572e331bd49c6fb77
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-11 13:57:41 +0100

    feat: initializeData has now error handling, _DATA functions are now imported, saveQuestionAnswer refactor

--- src/API/api.js
+++ src/API/api.js
@@ -1,75 +1,10 @@
-// TODO: remove generateUID if not used
-// import { generateUID } from './helpers.js';
-import { formatQuestion } from './helpers.js';
+import {
+  _getUsers,
+  _saveQuestion,
+  _saveQuestionAnswer,
+  _getQuestions,
+} from '../_DATA';
 
-import { users } from '../DB/users.js';
-import { questions } from '../DB/questions.js';
-
-export function _getUsers () {
-  return new Promise((res, rej) => {
-    setTimeout(() => res({...users}), 1000)
-  })
-}
-
-export function _getQuestions () {
-  return new Promise((res, rej) => {
-    setTimeout(() => res({...questions}), 1000)
-  })
-}
-
-export function _saveQuestion (question) {
-  return new Promise((res, rej) => {
-    const authedUser = question.author;
-    const formattedQuestion = formatQuestion(question);
-
-    setTimeout(() => {
-      questions = {
-        ...questions,
-        [formattedQuestion.id]: formattedQuestion
-      }
-      
-      users = {
-        ...users,
-        [authedUser]: {
-          ...users[authedUser],
-          questions: users[authedUser].questions.concat([formattedQuestion.id])
-        }
-      }
-
-      res(formattedQuestion)
-    }, 1000)
-  })
-}
-
-export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      users = {
-        ...users,
-        [authedUser]: {
-          ...users[authedUser],
-          answers: {
-            ...users[authedUser].answers,
-            [qid]: answer
-          }
-        }
-      }
-
-      questions = {
-        ...questions,
-        [qid]: {
-          ...questions[qid],
-          [answer]: {
-            ...questions[qid][answer],
-            votes: questions[qid][answer].votes.concat([authedUser])
-          }
-        }
-      }
-
-      res()
-    }, 500)
-  })
-}
 
 export function initializeData() {
   return Promise.all(
@@ -91,16 +26,30 @@ export function initializeData() {
     )
   ).catch(
     error => {
-      /* TODO: add routing to error page */
-      console.error('Error: ', error)
+      console.error(
+        {
+          error,
+          msg: 'something went wrong while initializing the data, please try again later',
+        }
+      )
     }
   );
 }
 
-export function saveQuestion(question) {
-  return _saveQuestion(question);
+export function saveQuestion( question ) {
+  return _saveQuestion( question );
 }
 
-export function saveQuestionAnswer(authUser, qid, answer) {
-  return _saveQuestionAnswer({ authUser, qid, answer });
+export function saveQuestionAnswer(
+  authedUser,
+  qid,
+  answer,
+) {
+  return _saveQuestionAnswer(
+    {
+      authedUser,
+      qid,
+      answer,
+    }
+  );
 }
