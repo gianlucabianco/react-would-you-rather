227b5675480e2e4f5b4d27b2ee5d2f6c11b4c9aa
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-11 14:03:03 +0100

    feat: users has now addAnswerToUser and handleSaveQuestionAnswer actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,8 @@
+import { saveQuestionAnswer } from '../API/api';
+import { addAnswerToQuestion } from '../actions/questions';
+
 export const GET_USERS = 'GET_USERS';
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
 
 export function getUsers(
   users,
@@ -6,5 +10,56 @@ export function getUsers(
   return {
     type: GET_USERS,
     users
+  };
+}
+
+function addAnswerToUser(
+  authUser,
+  questionId,
+  answer,
+) {
+  return {
+    type: ADD_ANSWER_TO_USER,
+    authUser,
+    qid: questionId,
+    answer
+  };
+}
+
+export function handleSaveQuestionAnswer(
+  authUser,
+  questionId,
+  answer,
+) {
+
+  return dispatch => {
+
+    dispatch(
+      addAnswerToUser(
+        authUser,
+        questionId,
+        answer,
+      )
+    );
+    dispatch(
+      addAnswerToQuestion(
+        authUser,
+        questionId,
+        answer,
+      )
+    );
+
+    return saveQuestionAnswer(
+        authUser,
+        questionId,
+        answer,
+    ).catch(e => {
+      console.error(
+        {
+          message: 'There is an error in handleSaveQuestionAnswer',
+          error: e,
+        }
+      )
+    });
   };
 }
