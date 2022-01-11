1e85be23e4e239eab5166a77679c1a773317fcab
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-11 14:04:53 +0100

    feat: questions has now addAnswerToQuestion, addQuestion and handleSaveQuestion actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,8 @@
+import { saveQuestion } from '../API/api';
+
 export const GET_QUESTIONS = 'GET_QUESTIONS';
+export const ADD_ANSWER_TO_QUESTION = 'ADD_ANSWER_TO_QUESTION';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function getQuestions(
     questions,
@@ -6,5 +10,51 @@ export function getQuestions(
   return {
     type: GET_QUESTIONS,
     questions
+  };
+}
+
+export function addAnswerToQuestion(
+  authUser,
+  questionId,
+  answer
+) {
+  return {
+    type: ADD_ANSWER_TO_QUESTION,
+    authUser,
+    qid: questionId,
+    answer
+  };
+}
+
+function addQuestion(
+  question,
+) {
+  return {
+    type: ADD_QUESTION,
+    question
+  };
+}
+
+export function handleSaveQuestion(
+  optionOneText,
+  optionTwoText,
+  author,
+) {
+  return dispatch => {
+    return saveQuestion(
+      {
+        optionOneText,
+        optionTwoText,
+        author,
+      }
+    ).then(
+      question => {
+        dispatch(
+          addQuestion(
+            question
+          )
+        );
+      }
+    );
   };
 }
