9c7fa04b045d07452bab6ece3588022b66a645be
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-15 11:16:27 +0100

    feat: users actions have now addQuestionToUser

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,6 +3,7 @@ import { addAnswerToQuestion } from '../actions/questions';
 
 export const GET_USERS = 'GET_USERS';
 export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
 
 export function getUsers(
   users,
@@ -61,5 +62,18 @@ export function handleSaveQuestionAnswer(
         }
       )
     });
+  };
+}
+
+export function addQuestionToUser(
+  {
+    id,
+    author,
+  },
+) {
+  return {
+    type: ADD_QUESTION_TO_USER,
+    id,
+    author
   };
 }
