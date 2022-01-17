c7d0e872a2183f437728ece5d89343b39b5d3e59
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-17 18:34:11 +0100

    feat: data added to credentials, credentials has now _addCredentials

--- src/DB/credentials.js
+++ src/DB/credentials.js
@@ -1,7 +1,41 @@
 /*
 This is a fake database that stores the credentials for the user.
-In a real projects, this would be stored in a database.
-When updated, this file is not pushed to the repository (gitignore).
+In a real projects, this would be stored in a database (these data should not be exposed).
 */
 
-export const credentials = [];
+export let credentials = [
+    {
+        id: 'sarahedo',
+        name: 'Sarah Edo',
+        password: 'test',
+    },
+    {
+        id: 'tylermcginnis',
+        name: 'Tyler McGinnis',
+        password: 'test',
+    },
+    {
+        id: 'johndoe',
+        name: 'John Doe',
+        password: 'test',
+    },
+];
+
+export const _addCredentials = (newCredential) => {
+
+    const indexOfUser = credentials.findIndex(
+        credential => credential.id === newCredential.id
+    );
+
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+
+            if (indexOfUser === -1)
+                credentials.push(newCredential);
+            else
+                credentials[indexOfUser] = newCredential;
+
+            res();
+        }, 1000);
+    });
+}
