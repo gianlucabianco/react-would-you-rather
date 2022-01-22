de5ed69988fdcca0954c9fb9471373148874f4d5
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-22 17:45:42 +0100

    chore: import statement less explicit for consistency

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './components/App/App.js';
+import App from './components/App/App';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/index.js';
+import rootReducer from './reducers/index';
 import middleware from './middleware';
 
 const store = createStore(rootReducer, middleware);
