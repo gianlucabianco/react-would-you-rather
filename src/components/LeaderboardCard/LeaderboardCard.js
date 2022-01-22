37cce0c8ed0b1d1d167d79ba6d3a85341dcdcb99
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-22 17:34:15 +0100

    chore: component moved into its own folder

---
+++ src/components/LeaderboardCard/LeaderboardCard.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '../Avatar/Avatar';
+import './LeaderboardCard.css';
+
+class LeaderboardCard extends React.Component {
+
+    render() {
+
+        const {
+            name,
+            avatarURL,
+            scoreAnsered,
+            scoreAsked,
+            totalScore,
+            placing,
+        } = this.props;
+
+        return (
+            <div className="leaderboard-card">
+                <div className="leaderboard-left">
+                    <Avatar
+                        url={avatarURL}
+                    />
+                    <span>
+                        {name}
+                    </span>
+                </div>
+                <div className="leaderboard-right">
+                    <div className="leaderboard-right-top">
+                        {
+                            placing < 4 && (
+                                <div className={
+                                    `
+                                        leaderboard-tag
+                                        ${placing === 1 ? 'first-place' : ''}
+                                        ${placing === 2 ? 'second-place' : ''}
+                                        ${placing === 3 ? 'third-place' : ''}
+                                    `
+                                }>
+                                    <span>{placing}</span> place
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="leaderboard-right-content">
+                        <span>
+                            Number of answer: {scoreAnsered}
+                        </span>
+                        <span>
+                            Number of questions: {scoreAsked}
+                        </span>
+                    </div>
+                    <div className="leaderboard-right-bottom">
+                        <span>
+                            Total score: {totalScore}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+LeaderboardCard.propTypes = {
+    avatarURL: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    placing: PropTypes.number.isRequired,
+    scoreAnsered: PropTypes.number.isRequired,
+    scoreAsked: PropTypes.number.isRequired,
+    totalScore: PropTypes.number.isRequired,
+};
+
+export default LeaderboardCard;
