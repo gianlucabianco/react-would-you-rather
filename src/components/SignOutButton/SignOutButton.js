4e04669ce089952aa00edead1555eaf66cb39e2f
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-22 17:34:44 +0100

    chore: component moved into its own folder

---
+++ src/components/SignOutButton/SignOutButton.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './SignOutButton.css';
+import Avatar from '../Avatar/Avatar';
+
+class SignOutButton extends Component {
+
+    render() {
+
+        const {
+            userUrl,
+            userName,
+        } = this.props;
+
+        return (
+            <div className="signout-button">
+                <div className="avatar-wrapper" >
+                    <Avatar
+                        url={ userUrl }
+                        size={'40px'}
+                    />
+                    <span className="user-name">
+                        { userName }
+                    </span>
+                </div>
+                <span className="signout-cta">
+                    Sign out
+                </span>
+            </div>
+        );
+
+    };
+
+};
+
+SignOutButton.propTypes = {
+    userUrl: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+};
+
+export default SignOutButton;
