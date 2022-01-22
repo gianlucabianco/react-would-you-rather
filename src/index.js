4839349b46f1dab64c9fb1246ee026628cd06a8f
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-22 17:30:56 +0100

    chore: import statement adjusted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './components/App.js';
+import App from './components/App/App.js';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
