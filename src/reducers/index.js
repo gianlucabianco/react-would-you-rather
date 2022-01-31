7a90800cfeeda8e5c2b15088b86f07634366937d
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-31 15:33:56 +0100

    feat: index.js combines now filters.js reducers also

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,15 @@ import { combineReducers } from 'redux';
 import authUser from './authUser';
 import questions from './questions';
 import users from './users';
+import {
+  isNotAnswerFilter,
+  isAnswerFilter,
+} from './filters';
 
 export default combineReducers({
   authUser,
   questions,
-  users
+  users,
+  isNotAnswerFilter,
+  isAnswerFilter,
 });
