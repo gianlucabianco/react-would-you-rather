dc154ee7bd5c9a8ff14d03a1ba2eb8c578946d79
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-31 08:57:12 +0100

    feat: AnswerTag is now component

---
+++ src/components/AnswerTag/AnswerTag.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './AnswerTag.css';
+
+class AnswerTag extends React.Component {
+
+    render() {
+
+        return (
+            <div className="answer-tag">
+                Your answer
+            </div>
+        );
+
+    };
+
+};
+
+export default AnswerTag;
