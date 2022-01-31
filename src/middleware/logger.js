5f46ff08bf3dde43bf6b332f14922ff92e63334d
Gianluca Bianco <ciao@gianlucabianco.dev>
2022-01-31 15:50:18 +0100

    chore: linting

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,21 +1,25 @@
 const logger = store => next => action => {
-    console.group(action.type);
-    console.log(
-      {
-        action,
-      }
-    );
 
-    const result = next(action);
+  console.group(action.type);
 
-    console.log(
-      {
-        newState: store.getState(),
-      }
-    );
-    console.groupEnd();
+  console.log(
+    {
+      action,
+    }
+  );
 
-    return result;
-  };
-  
-  export default logger;
+  const result = next(action);
+
+  console.log(
+    {
+      newState: store.getState(),
+    }
+  );
+
+  console.groupEnd();
+
+  return result;
+
+};
+
+export default logger;
